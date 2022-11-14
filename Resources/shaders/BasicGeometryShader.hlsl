990bca8486868db6c65e78504f5abeca361412a4
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-11-14 23:10:16 +0900

    02_03_end

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -5,9 +5,9 @@
 //	float4 pos : SV_POSITION;
 //};
 
-[maxvertexcount(6)]
+[maxvertexcount(3)]
 void main(
-	triangle VSOutput input[3] : SV_POSITION, 
+	point VSOutput input[1] : SV_POSITION,
 	// トライアングルストリーム
 	inout TriangleStream< GSOutput > output
 	// 点ストリーム
@@ -16,24 +16,19 @@ void main(
 	//inout LineStream< GSOutput > output
 )
 {
-	// 三角1
-	for (uint i = 0; i < 3; i++) {
-		GSOutput element;
-		element.svpos = input[i].svpos;
-		element.normal = input[i].normal;
-		element.uv = input[i].uv;
-		output.Append(element);
+	GSOutput element;
+	// 共通	
+	element.normal = input[0].normal;
+	element.uv = input[0].uv;
+	// 点1
+	element.svpos = input[0].svpos;
+	output.Append(element);
+	// 点2
+	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0.0f, 0.0f);
+	output.Append(element);
+	// 点3
+	element.svpos = input[0].svpos + float4(10.0f, 0.0f, 0.0f, 0.0f);
+	output.Append(element);
 
-	}
-	// 現在のストリップを終了
-	output.RestartStrip();
-	// 三角2
-	for (uint i = 0; i < 3; i++) {
-		GSOutput element;
-		element.svpos = input[i].svpos + float4(20.0f,0.0f,0.0f,0.0f);
-		element.normal = input[i].normal;
-		element.uv = input[i].uv * 5.0f;
-		output.Append(element);
 
-	}
 }
