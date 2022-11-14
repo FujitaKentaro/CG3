ac4b8349784a7a247b8385743cf59fd9856c741e
Fujita_Kentaro <k021g1261@g.neec.ac.jp>
2022-11-15 00:47:47 +0900

    02_04_end

--- Resources/shaders/BasicGeometryShader.hlsl
+++ Resources/shaders/BasicGeometryShader.hlsl
@@ -1,11 +1,28 @@
 #include "BasicShaderHeader.hlsli"
 
-//struct GSOutput
-//{
-//	float4 pos : SV_POSITION;
-//};
+// 四角形の頂点数
+static const uint vnum = 4;
 
-[maxvertexcount(3)]
+// センターからのオフセット
+static const float4 offset_array[vnum] =
+{
+	float4(-0.5f,-0.5f,0,0),// 左下
+	float4(-0.5f,+0.5f,0,0),// 左上
+	float4(+0.5f,-0.5f,0,0),// 右した
+	float4(+0.5f,+0.5f,0,0),// 右上
+};
+
+// 左上が0，0 右下が1，1
+static const float2 uv_array[vnum] =
+{
+	float2(0,1),// 左下
+	float2(0,0),// 左上
+	float2(1,1),// 右した
+	float2(1,0),// 右上
+};
+
+// 点の入力から、四角形を出力
+[maxvertexcount(vnum)]
 void main(
 	point VSOutput input[1] : SV_POSITION,
 	// トライアングルストリーム
@@ -17,18 +34,13 @@ void main(
 )
 {
 	GSOutput element;
-	// 共通	
-	element.normal = input[0].normal;
-	element.uv = input[0].uv;
-	// 点1
-	element.svpos = input[0].svpos;
-	output.Append(element);
-	// 点2
-	element.svpos = input[0].svpos + float4(10.0f, 10.0f, 0.0f, 0.0f);
-	output.Append(element);
-	// 点3
-	element.svpos = input[0].svpos + float4(10.0f, 0.0f, 0.0f, 0.0f);
-	output.Append(element);
-
-
+	//４点分回す
+	for (uint i = 0; i < vnum; i++) {
+		// ワールド座標ベースで、ずらす
+		element.svpos = input[0].pos + offset_array[i];
+		// ビュー、射影変換
+		element.svpos = mul(mat, element.svpos);
+		element.uv = uv_array[i];
+		output.Append(element);
+	}
 }
